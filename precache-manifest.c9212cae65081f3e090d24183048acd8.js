c03d3b1c6b52227b15b99cb2ca775052ec639b0b
John Running <john.a.running@gmail.com>
2019-02-10 15:17:40 -0500

    Updates

---
+++ precache-manifest.c9212cae65081f3e090d24183048acd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cef22fb7c357707b1346",
+    "url": "/React-Youtube-Video-Player/static/js/runtime~main.cef22fb7.js"
+  },
+  {
+    "revision": "e55143b55c09ff0b25f3",
+    "url": "/React-Youtube-Video-Player/static/js/main.e55143b5.chunk.js"
+  },
+  {
+    "revision": "c9aef50ce1e1f3a52a23",
+    "url": "/React-Youtube-Video-Player/static/js/1.c9aef50c.chunk.js"
+  },
+  {
+    "revision": "e55143b55c09ff0b25f3",
+    "url": "/React-Youtube-Video-Player/static/css/main.491ee330.chunk.css"
+  },
+  {
+    "revision": "6c2447afcfc14b4c2a53abd9335be9ec",
+    "url": "/React-Youtube-Video-Player/index.html"
+  }
+];
